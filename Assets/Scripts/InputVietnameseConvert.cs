5db4fb436dc3325ef2bbfdbee7a8314625c6a8f7
nguyentuananh <anhnt@ftech.ai>
2022-10-05 16:32:44 +0700

    vn

--- Assets/Scripts/InputVietnameseConvert.cs
+++ Assets/Scripts/InputVietnameseConvert.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ViToEn", order = 2)]
 public class InputVietnameseConvert : ScriptableObject
 {
-    public string engKey;
+    public List<string> engKey;
     public List<string> vietKey;
-
+    public Attitude attitude;
     public bool IsVietnamese(string inputStr) {
 
         foreach (var item in vietKey)
         {
-            if (inputStr == item) return true;
+            if (inputStr.Contains(item)) {
+                return true;
+            }
         }
         return false;
     }
     public string GetEnglishKey() {
         Debug.Log("Return English Key:" + engKey);
-        return engKey;
+        string eng = engKey[Random.Range(0, engKey.Count)];
+        return eng;
     }
+    public enum Attitude { Normal,Happy,Sad,Angry,Confuse}
 }
